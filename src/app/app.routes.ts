36a609b08ef84ddaefc6d77e6f57aa889b52d2e2
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2024-03-20 20:08:59 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { InboxComponent } from './inbox/inbox.component';
 import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
+    { path: '', component: LoginComponent},
     { path: 'login', component: LoginComponent },
     { path: 'inbox', component: InboxComponent },
     { path: 'signup', component: SignupComponent },
