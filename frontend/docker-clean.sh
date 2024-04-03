8367aa2f0842e1ed733b2b1d210bff267b7b494f
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2024-04-02 19:01:16 -0600

    bash & change on signup

---
+++ frontend/docker-clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Guardar el directorio actual
+current_dir=$(pwd)
+
+# Detener y eliminar todos los contenedores
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
+# Eliminar todas las imágenes no utilizadas
+docker image prune -af
+
+# Eliminar todos los volúmenes no utilizados
+docker volume prune -f
+
+# Moverse a la raíz del proyecto (cambia "/ruta/a/la/raiz" por la ruta adecuada)
+cd ..
+
+# Levantar los contenedores utilizando docker-compose
+docker-compose up 
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { GlobalService } from '../services/global.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, HttpClientModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  registered_users: any[] = [];
+
   user: string = '';
 
   password: string = '';
@@ -26,15 +29,53 @@ export class SignupComponent {
 
   isInfo = false;
 
+  usernameExists: boolean = false;
+
   constructor(
     private router: Router,
-    private globalService: GlobalService
+    private globalService: GlobalService,
+    private http: HttpClient
   ) { }
+
+  ngOnInit(): void {
+    console.log('funciona el on init')
+    this.loadUsers();
+    console.log('llamo a load users')
+  }
+
+  loadUsers(): void {
+    this.http.get<any[]>('http://localhost:3000/users').subscribe({
+      next: (data) => {
+        this.registered_users = data;
+        console.log('usuarios encontrados con exito! Estos son:\n', this.registered_users)
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+  }
 
   navigateTo(route: string): void {
     if (route === '/home') {
       if (this.user && this.password) {
-        this.router.navigateByUrl(route);
+        if (this.usernameExists) {
+          console.log('Username is taken.');
+          this.showAvailableToast = false;
+          this.isError = true;
+          setTimeout(() => {
+            this.showToast = false;
+            this.isError = false;
+          }, 3000);
+        } else {
+          // Usuario si está disponible se genera llave publica y privada
+          //PRIVADA
+
+          // DOWNLOAD PRIVADA EN TXT
+
+          //PUBLICA
+
+          this.router.navigateByUrl(route);
+        }
       } else {
         this.showToast = true;
         this.isError = true;
@@ -46,30 +87,11 @@ export class SignupComponent {
   }
 
   updateUser(value: string): void {
-    if (1 == 1) {
-      // TODO -- QUERY consultar existencia de username en base de datos
-      // expected return: 1 = exists, 0 = no match.
-      this.showAvailableToast = true;
-      this.isInfo = true;
-      setTimeout(() => this.showToast = false, 3000);
-      this.isInfo = false;
-    } else {
-      this.showAvailableToast = true;
-      this.isError = true;
-      setTimeout(() => this.showToast = false, 3000);
-      this.isError = false;
-    }
-    this.globalService.username = this.user;
+    this.usernameExists = this.registered_users.some(user => user.username === value);
+    this.user = value;
   }
 
   updatePassword(value: string): void {
-    if (1 == 1) {
-      this.password = value;
-    } else {
-      this.showInvalidPasswordToast = true;
-      this.isError = true;
-      setTimeout(() => this.showToast = false, 3000);
-      this.isError = false;
-    }
+    this.password = value;
   }
 }
