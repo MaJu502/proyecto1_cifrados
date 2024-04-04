a1f4419e712c612c39b9e3ff6598bc1aa1f0ef0a
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2024-04-04 14:44:00 -0600

    chat box

--- frontend/src/app/groups/groups.component.ts
+++ frontend/src/app/groups/groups.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
+import { GlobalService } from '../services/global.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-groups',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, FormsModule, NgIf, HttpClientModule, NgFor],
   templateUrl: './groups.component.html',
   styleUrl: './groups.component.scss'
 })
-export class GroupsComponent {
+export class GroupsComponent implements OnInit {
+  registered_groups: any[] = [];
+
+  username: string = '';
+
+  privateKey: string = '';
+
+  messages: any[] = []; // Nueva propiedad para almacenar los mensajes
+
+  constructor(private route: ActivatedRoute, private globalService: GlobalService, private http: HttpClient) { }
+
+  loadGroups(): void {
+    this.http.get<any[]>('http://localhost:3000/groups').subscribe({
+      next: (data) => {
+        this.registered_groups = data;
+        console.log('grupos encontrados con exito! Estos son:\n', this.registered_groups)
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+    console.log('termina loadgroups')
+  }
+
+  loadGroupMessages(groupID: string): void {
+    this.http.get<any[]>('http://localhost:3000/messages/groups/' + groupID).subscribe({
+      next: (data) => {
+        this.messages = data;
+        console.log('mensajes encontrados con exito! Estos son:\n', this.messages)
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+    console.log('termina loadGroupMessages')
+  }
+
+  ngOnInit(): void {
+    this.username = localStorage.getItem('username') || '';
+    this.loadGroups();
+  }
 
 }
--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
 import { GlobalService } from '../services/global.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import * as Forge from 'node-forge';
 
 
 @Component({
@@ -23,6 +21,8 @@ export class UsersComponent implements OnInit {
 
   privateKey: string = '';
 
+  messages: any[] = [];
+
   constructor(private route: ActivatedRoute, private globalService: GlobalService, private http: HttpClient) { }
 
   loadUsers(): void {
@@ -38,10 +38,22 @@ export class UsersComponent implements OnInit {
     console.log('termina loadusers')
   }
 
+  loadUserMessages(origin: string, dest: string): void {
+    this.http.get<any[]>('http://localhost:3000/messages/' + origin + '/users/' + dest).subscribe({
+      next: (data) => {
+        this.messages = data;
+        console.log('mensajes encontrados con exito! Estos son:\n', this.messages)
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+    console.log('termina loadUserMessages')
+  }
+
   ngOnInit(): void {
-    console.log('funciona el on init')
     this.loadUsers();
-    console.log('llamo a load users')
+    this.username = localStorage.getItem('username') || '';
   }
 
 }
