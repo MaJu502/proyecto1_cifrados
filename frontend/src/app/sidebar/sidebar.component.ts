221d530a1c278706218f96c4fd4aca5e0a1a7f37
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2024-04-07 16:02:16 -0600

    cambiar contraseña y agregar grupo necesitan agregarles el api en función del frontend

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NgIf, CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import * as Forge from 'node-forge';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,13 +12,18 @@ import { Router } from '@angular/router';
 })
 export class SidebarComponent {
   @Input() username: string | null = null;
+
   @Input() privateKey: string | null = null;
-  
+
   isCollapsed = false;
 
   dropdownOpen: string | null = null;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
+
+  private_key: string = '';
+
+  public_key: string = '';
 
   toggleSidebar(): void {
     this.isCollapsed = !this.isCollapsed;
@@ -30,5 +36,28 @@ export class SidebarComponent {
 
   navigateTo(route: string): void {
     this.router.navigateByUrl(route);
+  }
+
+  regenerarLlaves(): void {
+    console.log("Regenerando llaves...");
+    // REGENERAR LLAVES AQUI
+    let pair = Forge.pki.rsa.generateKeyPair(2048, 0x10001);
+    let pemPrivateKey = Forge.pki.privateKeyToPem(pair.privateKey);
+    let pemPublicKey = Forge.pki.publicKeyToPem(pair.publicKey);
+    let publicKeyStr = pemPublicKey.replace('-----BEGIN PUBLIC KEY-----', '').replace('-----END PUBLIC KEY-----', '').replace(/\n/g, '');
+    let privateKeyStr = pemPrivateKey.replace('-----BEGIN RSA PRIVATE KEY-----', '').replace('-----END RSA PRIVATE KEY-----', '').replace(/\n/g, '');
+    this.private_key = privateKeyStr.replace(/\r/g, '');
+    this.public_key = publicKeyStr.replace(/\r/g, '');
+
+    // API PARA CAMBIAR LA LLAVE PUBLICA AQUIIII
+
+    const blob = new Blob([this.private_key], { type: 'text/plain' });
+    const anchor = document.createElement('a');
+    anchor.download = 'private_key.txt';
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.style.display = 'none';
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
   }
 }
